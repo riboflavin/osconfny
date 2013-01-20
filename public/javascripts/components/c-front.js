dfc8a221ce708501c157dbe868528653671e8bf8
Justin Dunham <github@justindunham.net>
2013-01-19 22:02:04 -0500

    v3 of the site

--- public/javascripts/components/c-front.js
+++ public/javascripts/components/c-front.js
@@ -1,37 +1,34 @@
+var fxopts = {'effect':'slide','duration':'300'};
+
 $(document).ready(function(){
 
 $('.section').css('display','none');
-$('.action').bind('click',function(){
+$('#about_s').css('display','');
+$('.brand').bind('click',function(){
+	$('#about').click();
+});
+
+$('#navbar li a').bind('click',function(){
 	var show_section = '#' + $(this).attr('id') + '_s';
 	var $show_section = $(show_section);
 	var $visible = $('.section:visible');
-
-	var fxopts = {'effect':'slide','duration':'300'};
-
 	if (typeof($visible.get(0)) === 'undefined')  
 	{
-	//if nothing is showing, just show the section
+		//if nothing is showing, just show the section
 		$show_section.show(fxopts);
 	}
 	else if (!($show_section.is(':visible')))
 	//we know something is showing. if the section isn't, hide everything else and then show the section
 	{
 		$visible.hide('slide',function(){
-		$show_section.show(fxopts);			
+		$show_section.show(fxopts);      
 		});
 	}
 	//do nothing if the section is already showing
 	//regardless of whether it is showing alongside other things (since that should never happen)
 });
 
-$('#modal_close').bind('click',function(){
-	$('#signup_form').modal('toggle');
-});
-$('.formaction').bind('click',function(){
-	$('#talk').attr('checked',false);
-	$('#talk').bind('click',function(){$('#talkdetails').toggle()}); 
-	$('#signup_form').modal('toggle');
-});
+$('#talkbutton').bind('click',function(){$('#talksubmit').show(fxopts);});
 
 $('#submit_button').bind('click',function(){
 	$('#submit_button').text("Working...").removeClass('btn-danger').removeClass('btn-primary').removeClass('btn-success');

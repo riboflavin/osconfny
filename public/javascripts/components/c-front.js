ca0a070c774876a77ee5f9974a5f4b6ef044c2f8
riboflavin <justin@10gen>
2013-01-22 23:53:10 -0500

    add blog and fsp

--- public/javascripts/components/c-front.js
+++ public/javascripts/components/c-front.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
 $('.section').css('display','none');
 $('#about_s').css('display','');
+
 $('.brand').bind('click',function(){
 	$('#about').click();
 });
@@ -28,25 +29,11 @@ $('#navbar li a').bind('click',function(){
 	//regardless of whether it is showing alongside other things (since that should never happen)
 });
 
-$('#talkbutton').bind('click',function(){$('#talksubmit').show(fxopts);});
-
-$('#submit_button').bind('click',function(){
-	$('#submit_button').text("Working...").removeClass('btn-danger').removeClass('btn-primary').removeClass('btn-success');
-	$('#email_address').parent().removeClass('error').removeClass('success');
-	$.post("/",$('#signup_form_data').serialize(),function(data){
-		console.log(data.response);
-		if (data.response == 'OK') {
-			$('#thanks').css('display','');
-			$('#thanks').text("Thanks! We'll be in touch.");
-			$('#submit_button').removeClass('btn-danger').removeClass('btn-primary').addClass('btn-success').text("Thanks!");
-			$('#email_address').parent().removeClass('error').addClass('success');
-		}
-		else {
-			$('#thanks').text(data.response);
-			$('#thanks').css('display','');
-			$('#submit_button').removeClass('btn-primary').removeClass('btn-success').addClass('btn-danger').text("Submit");
-			$('#email_address').parent().removeClass('success').addClass('error');
-		}
+$('#fsp').bind('click',function(){
+	$('.section:visible').hide('slide',function(){
+		$('#fsp_s').show(fxopts);
 	});
 });
+
+
 });
